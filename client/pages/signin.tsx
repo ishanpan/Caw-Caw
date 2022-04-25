e1df234914228036d4383cf3b3f0e6895e6421c8
ishanpan <ishanpandey2905@gmail.com>
2022-04-25 23:51:41 +0530

    feat:homepage

---
+++ client/pages/signin.tsx
@@ -0,0 +1,41 @@
+import { NextPage } from "next";
+import styles from "../styles/signup.module.scss";
+import { Input, Spacer, Button, createTheme, Text } from "@nextui-org/react";
+import Image from "next/image";
+import crowLogo from "../public/303906.svg";
+import Select from "react-select";
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
+import Auth from "../components/Auth";
+
+interface IFormInput {
+  Email: string;
+  Password: string;
+}
+
+const SignIn: NextPage = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    clearErrors,
+  } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    console.log(data);
+  };
+  return (
+    <Auth
+      handleSubmit={handleSubmit}
+      onSubmit={onSubmit}
+      control={control}
+      errors={errors}
+    ></Auth>
+  );
+};
+
+export default SignIn;
+
+function handleSubmit(
+  onSubmit: SubmitHandler<IFormInput>
+): import("react").FormEventHandler<HTMLFormElement> | undefined {
+  throw new Error("Function not implemented.");
+}
--- client/pages/signup.tsx
+++ client/pages/signup.tsx
@@ -1,32 +1,7 @@
-import { NextPage } from "next";
-import styles from "../styles/signup.module.scss";
-import { Input, Spacer, Button, createTheme, Text } from "@nextui-org/react";
-import Image from "next/image";
-import crowLogo from "../public/303906.svg";
+import React from "react";
 
-const SignUp: NextPage = () => {
-  return (
-    <div className={styles.base}>
-      <div className={styles.logo}>
-        <Image src={crowLogo} alt="Logo"></Image>
-      </div>
-      <div className={styles.box}>
-        <h2 className={styles.h2Text}>Sign in to your account</h2>
-        <Spacer y={1.75} />
-        <Input size="xl" labelPlaceholder="Email" />
-        <Spacer y={2} />
-        <Input.Password
-          size="xl"
-          labelPlaceholder="Password"
-          initialValue="nextui123"
-        />
-        <Spacer y={1.75} />
-        <Button color="secondary" size="xl">
-          Sign In
-        </Button>
-      </div>
-    </div>
-  );
-};
+function SignUp() {
+  return <div>signup</div>;
+}
 
 export default SignUp;
